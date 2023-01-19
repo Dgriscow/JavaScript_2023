443e7c9ec67164a32b29399c823c56cd37a50410
Dgriscow <dgrischow@students.mchenry.edu>
2023-01-19 15:08:24 -0600

    first lab done

---
+++ assessment-01/scripts/javascript.js
@@ -0,0 +1,20 @@
+
+function mollySelector(){
+    document.getElementById("PetStorage").style.display='block';
+
+    document.getElementById("PetParagraph").innerHTML = "This is molly, currently she is the oldest pet in the house. She loves laying around all day and sitting in the biggest chairs in the living room, taking them all for herself. she is a little princess in the small form of a dog. ";
+    document.getElementById("PetStorage").src="images/molly.jpeg";
+    document.getElementById("PetParagraph").style.color = "white";
+    document.getElementById("PetStorage").style.width = "450px";
+}
+
+function blueSelector(){
+    document.getElementById("PetStorage").style.display='block';
+    
+    document.getElementById("PetParagraph").innerHTML = "Blue is the middle child of the family. She is a bearded dragon, and she is named after the velociraptor from jurassic park. she recently just got her cage improved and she has been super happy ever since. she always has a view of the downstairs TV, so she is always nearby people.";
+    document.getElementById("PetStorage").src="images/bl.jpg";
+    document.getElementById("PetParagraph").style.color = "white";
+    document.getElementById("PetStorage").style.width = "450px";
+
+}
+
